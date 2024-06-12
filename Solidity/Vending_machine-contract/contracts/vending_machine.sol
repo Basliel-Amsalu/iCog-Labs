75f3c4822fbfbafef4ddce1b286cb5e5203b0738
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-06-12 16:08:07 +0300

    function get amount

--- Solidity/Vending_machine-contract/contracts/vending_machine.sol
+++ Solidity/Vending_machine-contract/contracts/vending_machine.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity ^0.5.11;
 
 contract vending_machine {
   address public owner;
   mapping (address => uint) public candyBarStock;
 
-  constructor() {
+  constructor() public {
     owner = msg.sender;
     candyBarStock[address(this)] = 50;
   }
 
-}
+  function getAmount() public view returns (uint){
+    return candyBarStock[address(this)];
+  }
+  
+
+
