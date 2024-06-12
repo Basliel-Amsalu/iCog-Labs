e7544199014c55baf611740ae45274a32c44f6fc
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-06-12 16:49:14 +0300

    test for purchase function

--- Solidity/Vending_machine-contract/test/vending_machine.test.js
+++ Solidity/Vending_machine-contract/test/vending_machine.test.js
@@ -5,8 +5,17 @@ contract("vending_machine", (accounts) => {
     contractInstance = await vendingMachine.deployed();
   });
 
-  it("maket sure that starting amount of the vending machine is 50", async () => {
+  it("makes sure that starting amount of the vending machine is 50", async () => {
     let amount = await contractInstance.getAmount();
     assert.equal(amount, 50, "initial amount should be 50");
+  });
+
+  it("allows purcahse", async () => {
+    await contractInstance.purchase(1, {
+      from: accounts[0],
+      value: web3.utils.toWei("5", "ether"),
+    });
+    let amount = await contractInstance.getAmount();
+    assert.equal(amount, 49, "amount should be 49 after purchase");
   });
 });
