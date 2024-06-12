dfe50f50dd85a48684426708afa02c4fec802eca
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-06-12 16:08:45 +0300

    first test to test inital amount of candies in the vending machine

--- Solidity/Vending_machine-contract/test/vending_machine.test.js
+++ Solidity/Vending_machine-contract/test/vending_machine.test.js
@@ -1,3 +1,12 @@
-const vending_machine = artifacts.require("vending_machine");
+const vendingMachine = artifacts.require("vending_machine");
 
-contract("vending_machine", (accounts) => {});
+contract("vending_machine", (accounts) => {
+  before(async () => {
+    contractInstance = await vendingMachine.deployed();
+  });
+
+  it("maket sure that starting amount of the vending machine is 50", async () => {
+    let amount = await contractInstance.getAmount();
+    assert.equal(amount, 50, "initial amount should be 50");
+  });
+});
