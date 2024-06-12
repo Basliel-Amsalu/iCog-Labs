b5b49d0d2e7e1c5fc5090eca83795abb73b10497
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-06-12 16:26:35 +0300

    purchase function

--- Solidity/Vending_machine-contract/contracts/vending_machine.sol
+++ Solidity/Vending_machine-contract/contracts/vending_machine.sol
@@ -13,6 +13,12 @@ contract vending_machine {
   function getAmount() public view returns (uint){
     return candyBarStock[address(this)];
   }
-  
 
+  function purchase(uint amount) public payable{
+    require(msg.value >= amount * 2 ether, "2 ETH per candy");
+    require(candyBarStock[address(this)] >= amount, "Not enough candies in stock");
+    candyBarStock[address(this)] -= amount;
+    candyBarStock[msg.sender] += amount;
+  }
 
+}
